b0f996feafb77280cea2bb5b95050640fff0eb8f
Q118 <shelbyfish91@gmail.com>
2020-08-16 15:41:30 -0700

    initial styling for quiz page container

---
+++ client/src/components/Quiz/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import './style.css';
+import API from '../../utils/API';
+
+function Quiz() {
+
+}
+
+export default Quiz;
