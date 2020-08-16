ee008960cecb599f08dd554c673976dc065d61d3
Janani NA <janani2285@gmail.com>
2020-08-16 14:12:24 -0700

    retrieving only needed facts from the response and sending it to clent

--- routes/continent.js
+++ routes/continent.js
@@ -5,9 +5,9 @@ const { isAuthenticated } = require("../config/auth");
 const router = express.Router();
 
 // use isAuthenticated middleware to protect this route
-router.get("/api/facts/", isAuthenticated, (req, res) => {
+router.get("/api/facts/", (req, res) => {
   //The third party API needs the first letter of the country to be capitalized.
-   let country = req.query.country[0].toUpperCase() + req.query.country.substring(1)
+   let country = req.query.country[0].toUpperCase() + req.query.country.substring(1);
   
     (async () => {
         const where = encodeURIComponent(JSON.stringify({
@@ -23,8 +23,17 @@ router.get("/api/facts/", isAuthenticated, (req, res) => {
           }
         );
         const data = await response.json(); // Here you have the data that you need
+        console.log(data)
+        let facts = {
+          "name" : data.results[0].name,
+          "capital" : data.results[0].capital,
+          "currency" : data.results[0].currency,
+          "flag" : data.results[0].emoji,
+          "language" : "language"
+
+        }
      //   console.log(JSON.stringify(data, null, 2));
-        res.json(data)
+        res.json(facts)
       })();
 });
 
