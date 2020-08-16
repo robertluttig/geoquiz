9ad68bc30a891e1f02c538410e0d3548841a86a4
Janani NA <janani2285@gmail.com>
2020-08-15 20:03:13 -0700

    implemented route to add/update results

--- routes/results.js
+++ routes/results.js
@@ -4,27 +4,69 @@ const { isAuthenticated } = require("../config/auth");
 
 const router = express.Router();
 
-router.get("/api/results/:userid",isAuthenticated, (req, res) => {
-
-  db.Result.findOne({user: req.params.userid})
+router.get("/api/results/:userid", isAuthenticated, (req, res) => {
+  db.Result.findOne({ user: req.params.userid })
     .populate("user")
     .then((data) => {
       if (data) {
         res.json(data);
       } else {
-        res.status(404).send({ success: false, message: "No quiz results available for the requested user"});
+        res.status(404).send({
+          success: false,
+          message: "No quiz results available for the requested user",
+        });
       }
     })
     .catch((err) => res.status(400).send(err));
 });
 
-router.post("/api/result",isAuthenticated, (req, res) => {
-  db.Result.create(req.body)
+router.post("/api/result", isAuthenticated, (req, res) => {
+  db.Result.findOne({ user: req.body.user })
     .then((data) => {
       if (data) {
-        res.json(data);
+        //If this is not the first time the user is taking the quiz, then do a update
+        const continent = Object.keys(req.body.results);
+        let result = data.results;
+        let isContnentPresent = false;
+        result.forEach((obj) => {
+          const objKey = Object.keys(obj);
+          //checking for key match irrespective of case
+          if (objKey[0].toLowerCase() === continent[0].toLowerCase()) {
+            console.log("continent present");
+            //Both the objects uses same key with diffrent cases, so refering the objects with respective key
+            obj[objKey] = req.body.results[continent];
+            isContnentPresent = true;
+          }
+        });
+        if (!isContnentPresent) {
+          result.push(req.body.results);
+        }
+        db.Result.update({ user: req.body.user }, { $set: { results: result } })
+          .then((data) => {
+            if (data) {
+              res.json(data);
+            } else {
+              res.status(404).send({
+                success: false,
+                message: "No data recieved from database",
+              });
+            }
+          })
+          .catch((err) => res.status(400).send(err));
       } else {
-        res.status(404).send({ success: false, message: "No data recieved from database"});
+        //If user is taking the quiz for the first time, then do a create
+        db.Result.create(req.body)
+          .then((data) => {
+            if (data) {
+              res.json(data);
+            } else {
+              res.status(404).send({
+                success: false,
+                message: "No data recieved from database",
+              });
+            }
+          })
+          .catch((err) => res.status(400).send(err));
       }
     })
     .catch((err) => res.status(400).send(err));
