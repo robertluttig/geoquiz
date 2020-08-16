405c2ea37a95808e272fc7434e8f07033e4e1865
Q118 <shelbyfish91@gmail.com>
2020-08-16 15:59:57 -0700

    usestate on score, page, and question

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -1,23 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import './style.css';
-import API from '../../utils/API';
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./style.css";
+import API from "../../../../routes";
 
 function Quiz() {
-    const [questionState, setQuestionState] = useState({
-        started: false,
-        questions: [{
-          id: 1,
-          question: "",
-          choices: [],
-          imageUrl: "",
-          answer: 0,
-          stuAnswer: ""
-        }],
-        currentQuestion: 0,
-        feedback: ""
-      });
+	const [questionState, setQuestionState] = useState({
+		started: false,
+		questions: [
+			{
+				id: 1,
+				question: "",
+				imageUrl: "",
+				answer: 0,
+			},
+		],
+		currentQuestion: 0,
+	});
+
+	// state for controlling page being displayed
+	const [pageState, setPageState] = useState("quiz");
+
+	//state for the score
+    const [scoreState, setScoreState] = useState();
     
+    //load the quiz into state on page load
+    useEffect(() => {
+        getQuiz().then(res => {
+          res.data.questions.map(item => {
+            return item.answer = "";
+          });
+          setQuestionState({
+            ...questionState, questions: res.data.questions, timeLimit: res.data.timeLimit, title: res.data.title
+          });
+        })
+      }, []);
+
+
+
+
 
 }
 
