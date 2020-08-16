e2f4ba2f28f4e1d5ae6540b745633adf1206e717
Q118 <shelbyfish91@gmail.com>
2020-08-16 15:52:22 -0700

    setting initial state of quiz

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -4,6 +4,20 @@ import './style.css';
 import API from '../../utils/API';
 
 function Quiz() {
+    const [questionState, setQuestionState] = useState({
+        started: false,
+        questions: [{
+          id: 1,
+          question: "",
+          choices: [],
+          imageUrl: "",
+          answer: 0,
+          stuAnswer: ""
+        }],
+        currentQuestion: 0,
+        feedback: ""
+      });
+    
 
 }
 
