7997403de8ec9949bbc506bbec0a08c2e66f0552
Q118 <shelbyfish91@gmail.com>
2020-08-16 18:18:53 -0700

    styling of container to seprate out visually

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./style.css";
+import "./arrow.css";
 import API from "../../utils/API";
 import mapImage from "./temp-map.jpg";
 
@@ -54,31 +55,44 @@ function Quiz() {
 	// const myRender = (page) => {
 	// if (page === "quiz") {
 	return (
-		<div className="quiz-form-container container">
-			<div className="row text-center">
+		<div className="container">
+			<div className="row text-center title-container p-3">
 				<div className="col-sm">
-  <h2> 
-    {/* {questionState.title}  */}
-   (Name-of-Country) Quiz
-    </h2>
-          </div>
+					<h2>
+						{/* {questionState.title}  */}
+						(Name-of-Country) Quiz
+					</h2>
+				</div>
 			</div>
-			<div className="row">
+			<div className="row quiz-form-container">
 				<div className="col-sm">
-          <span className= "question-container">Where is Peru?</span>
-          </div>
-        <div className="col-sm">
-          This will display the map <br />
-    <img src= {mapImage} alt= "Map" height= "400" width = "250" />
-          </div>
-        <div className="col-sm">
-        <div className="row">
-          Hellow Row2 col3, row 1
-          </div>
-        <div className="row">
-          Hellow Row2 col2, row 2
-          </div>
-        </div>
+					<span className="question-container">Where is Peru?</span>
+				</div>
+				<div className="col-sm map-container">
+					<img src={mapImage} alt="Map" height="400" width="250" />
+				</div>
+				<div className="col-sm">
+          Click your Answer on the Map
+					<div className="row click-container mb-5">
+						<div id="arrowAnim">
+							<div class="arrowSliding">
+								<div class="arrow"></div>
+							</div>
+							<div class="arrowSliding delay1">
+								<div class="arrow"></div>
+							</div>
+							<div class="arrowSliding delay2">
+								<div class="arrow"></div>
+							</div>
+							<div class="arrowSliding delay3">
+								<div class="arrow"></div>
+							</div>
+						</div>
+					</div>
+					<div className="row next-container m-5 p-5">
+          <button type="button" className="btn btn-danger btn-lg"> <h3>Next Question</h3></button>
+            </div>
+				</div>
 			</div>
 		</div>
 	);
