4483b9a3ea5b2764bf71f4ff740cc16277c0e12c
Janani NA <janani2285@gmail.com>
2020-08-17 22:20:39 -0700

    Added globe gif

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,6 +3,9 @@ import { useHistory } from "react-router-dom";
 import logo from "./logo.svg";
 import "./home.css";
 import { useAuth } from "../../utils/auth";
+import Container from "../../components/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 function Home() {
   const { user, logout } = useAuth();
@@ -11,18 +14,26 @@ function Home() {
   const goToEditProfile = () => history.push("/profile");
 
   return (
-    <div className="Home">
-      <div className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
+    <Container className="Home">
+      <Row>
+        <Col>
+         <img src="https://media.giphy.com/media/mf8UbIDew7e8g/giphy.gif" className="Home-logo"></img> 
+        </Col>
+      </Row>
+     
+      <Row className="Home-header">     
         <h2>Welcome {user.email}</h2>
-      </div>
-      <p className="Home-intro">
+      </Row>
+      <Row className="Home-intro">
+        <Col>
         <button onClick={goToEditProfile}>Go to Profile</button>
         <button style={{ marginLeft: "1em" }} onClick={() => logout()}>
           Logout
         </button>
-      </p>
-    </div>
+        </Col>
+        
+      </Row>
+    </Container>
   );
 }
 
