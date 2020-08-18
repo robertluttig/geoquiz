b8ed6ba4c4368b32a625d1ae34c4948708e2c63a
Robert Luttig <robert.luttig@gmail.com>
2020-08-17 18:45:49 -0700

    updated login page styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import { Link, Redirect, useHistory } from "react-router-dom";
 import { useAuth } from "../utils/auth";
 import { Form, InputGroup } from "../components/LoginForm";
+import Button from 'react-bootstrap/Button';
+import Container from "../components/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
 
 const loginStyle = {
   display: "flex",
@@ -33,36 +39,40 @@ function Login() {
   };
 
   return (
-    <div style={loginStyle}>
-      <h1>Login</h1>
-      <Form onSubmit={handleFormSubmit}>
-        <InputGroup
-          id="email"
-          labelText="Email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <InputGroup
-          id="password"
-          labelText="Password"
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </Form>
-      <Link
-        style={{
-          marginTop: "1.5rem",
-          textAlign: "center",
-        }}
-        to="/signup"
-      >
-        Go to Signup
-      </Link>
+    <div className="page">
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto" className="createAccount">
+            <Card className="card" border="dark">
+              <h1><Badge variant="warning">Login</Badge></h1>
+              <Form>
+                <InputGroup
+                  id="email"
+                  labelText="Email"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <InputGroup
+                  id="password"
+                  labelText="Password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button variant="success" size="lg" block onClick={handleFormSubmit} type="submit">Submit</Button>
+              </Form>
+            </Card>
+          </Col>
+          <Col md="auto">
+            <Card className="card" border="dark">
+              <Button variant="success" size="lg" block href="/signup" >Go to Signup</Button>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
