e1ffdced23a3cea5719acdba03a666b9cfaccf74
Janani NA <janani2285@gmail.com>
2020-08-18 19:25:59 -0700

    made AJAX call to retrieve country by continent

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -8,97 +8,108 @@ import API from "../../utils/API";
 import mapImage from "./temp-map.jpg";
 
 function Quiz() {
-	const continent =  useParams().continent;
-	const [questionState, setQuestionState] = useState({
-		started: false,
-		questions: [
-			{
-				id: 1,
-				question: "",
-				imageUrl: "",
-				answer: 0,
-			},
-		],
-		currentQuestion: 0,
+  const continent = useParams().continent;
+  const [questionState, setQuestionState] = useState({
+    started: false,
+    questions: [
+      {
+        id: 1,
+        question: "",
+        imageUrl: "",
+        answer: 0,
+      },
+    ],
+    currentQuestion: 0,
+  });
+
+  // // state for controlling page being displayed
+  // const [pageState, setPageState] = useState("quiz");
+
+  // //state for the score
+  // const [scoreState, setScoreState] = useState();
+
+  // //load the quiz into state on page load
+  /* useEffect(() => {
+    getQuiz().then((res) => {
+      res.data.questions.map((item) => {
+        return (item.answer = "");
+      });
+      setQuestionState({
+        ...questionState,
+        questions: res.data.questions,
+        title: res.data.title,
+      });
+    });
+  }, []); */
+
+  useEffect(() => {
+	  API.getCountryByContinent(continent)
+	  .then((res) => {
+		console.log(res.data) // Country Array from response
 	});
+	//TODO: Make call gto map api to retrieve the map for the given continent
+  }, []);
 
-	// // state for controlling page being displayed
-	// const [pageState, setPageState] = useState("quiz");
+  const { id } = useParams();
+  // // get quiz by ID
+  const getQuiz = async () => {
+    const quiz = await API.getQuizById(id);
+    return quiz;
+  };
 
-	// //state for the score
-	// const [scoreState, setScoreState] = useState();
+  // const preventFormSubmit = (event) => {
+  //   event.preventDefault();
+  // }
 
-	// //load the quiz into state on page load
-	useEffect(() => {
-		getQuiz().then((res) => {
-			res.data.questions.map((item) => {
-				return (item.answer = "");
-			});
-			setQuestionState({
-				...questionState,
-				questions: res.data.questions,
-				title: res.data.title,
-			});
-		});
-	}, []);
-
-	const { id } = useParams();
-	// // get quiz by ID
-	const getQuiz = async () => {
-		const quiz = await API.getQuizById(id);
-		return quiz;
-	};
-
-	// const preventFormSubmit = (event) => {
-	//   event.preventDefault();
-	// }
-
-	// const myRender = (page) => {
-	// if (page === "quiz") {
-	return (
-		<div className="container">
-			<div className="row text-center  mr-3">
-				<div className="col-sm-12 title-container p-3">
-					<h2>
-						{/* {questionState.title}  */}
-						(Name-of-Country) Quiz
-					</h2>
-				</div>
-			</div>
-			<div className="row quiz-form-container pt-5">
-				<div className="col-sm-4">
-					<span className="question-container">Where is Peru?</span>
-				</div>
-				<div className="col-sm-4 map-container">
-					<img src={mapImage} alt="Map" height="400" width="250" />
-				</div>
-				<div className="col-sm-4">
+  // const myRender = (page) => {
+  // if (page === "quiz") {
+  return (
+    <div className="container">
+      <div className="row text-center  mr-3">
+        <div className="col-sm-12 title-container p-3">
+          <h2>
+            {/* {questionState.title}  */}
+            (Name-of-Country) Quiz
+          </h2>
+        </div>
+      </div>
+      <div className="row quiz-form-container pt-5">
+        <div className="col-sm-4">
+          <span className="question-container">Where is Peru?</span>
+        </div>
+        <div className="col-sm-4 map-container">
+          <img src={mapImage} alt="Map" height="400" width="250" />
+        </div>
+        <div className="col-sm-4">
           Click your Answer on the Map
-					<div className="row click-container mb-5">
-						<div id="arrowAnim">
-							<div class="arrowSliding">
-								<div class="arrow"></div>
-							</div>
-							<div class="arrowSliding delay1">
-								<div class="arrow"></div>
-							</div>
-							<div class="arrowSliding delay2">
-								<div class="arrow"></div>
-							</div>
-							<div class="arrowSliding delay3">
-								<div class="arrow"></div>
-							</div>
-						</div>
-					</div>
-					<div className="row next-container m-5 p-5">
-          <button type="button" className="btn btn-danger btn-lg"> <h3>Next Question</h3></button>
+          <div className="row click-container mb-5">
+            <div id="arrowAnim">
+              <div className="arrowSliding">
+                <div className="arrow"></div>
+              </div>
+              <div className="arrowSliding delay1">
+                <div className="arrow"></div>
+              </div>
+              <div className="arrowSliding delay2">
+                <div className="arrow"></div>
+              </div>
+              <div className="arrowSliding delay3">
+                <div className="arrow"></div>
+              </div>
             </div>
-				</div>
-			</div>
-		</div>
-	);
-	// }
-	// }
+          </div>
+          <div className="row next-container m-5 p-5">
+            <button type="button" className="btn btn-danger btn-lg">
+              {" "}
+              <h3>Next Question</h3>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  // }
+  // }
 }
 
 export default Quiz;
