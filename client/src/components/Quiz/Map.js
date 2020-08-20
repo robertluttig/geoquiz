0e1293bee506e6af1cf0efb5be55c9418510ead9
Jack Meier <jackson.meier423@gmail.com>
2020-08-20 13:57:49 -0700

    removed country names from map

--- client/src/components/Quiz/Map.js
+++ client/src/components/Quiz/Map.js
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 import Geocode from "react-geocode";
 
 const mapStyles = {
-    width: '32vw',
-    height: '400px'
+  width: "32vw",
+  height: "400px",
+  featureType: "all",
+  elementType: "labels",
+  stylers: [{ visibility: "off" }],
 };
+
+var customStyled = [
+  {
+    featureType: "all",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }],
+  },
+]; //(array shown above)
+
 const GOOGLE_MAP_API = "AIzaSyBcjTAI9kaG51E3Yuqh5HnTCz_k042szLk";
 
 export class MapContainer extends Component {
-    // const[location, setLocation] = useState({ });
-    state = {
-        longitude:0,
-        latitude:0
-    }
-    componentDidMount(){
-        Geocode.setApiKey(GOOGLE_MAP_API);
-        Geocode.setLanguage("en");
-        Geocode.fromAddress(this.props.continent).then(
-            response => {
-                const { lat, lng } = response.results[0].geometry.location;
-                this.setState({ latitude: lat, longitude: lng })
-                console.log(lat, lng);
-            },
-            error => {
-                console.error(error);
-            }
-        );
-    }
-    render() {
-        return (
-            <Map
-                google={this.props.google}
-                zoom={3}
-                style={mapStyles}
-                center={{
-                    lat: this.state.latitude,
-                    lng: this.state.longitude
-                }}
-            />
-        );
-    }
+  // const[location, setLocation] = useState({ });
+  state = {
+    longitude: 0,
+    latitude: 0,
+  };
+  componentDidMount() {
+    Geocode.setApiKey(GOOGLE_MAP_API);
+    Geocode.setLanguage("en");
+    Geocode.fromAddress(this.props.continent).then(
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        this.setState({ latitude: lat, longitude: lng });
+        console.log(lat, lng);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={3}
+        style={mapStyles}
+        styles={customStyled}
+        featureType="all"
+        center={{
+          lat: this.state.latitude,
+          lng: this.state.longitude,
+        }}
+      />
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-    apiKey: GOOGLE_MAP_API
+  apiKey: GOOGLE_MAP_API,
 })(MapContainer);
