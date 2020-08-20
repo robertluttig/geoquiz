fefff47ea2c9e036d304ec1dc140ec24844a089b
Q118 <shelbyfish91@gmail.com>
2020-08-20 13:14:34 -0700

    arrows facing towards the map

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -9,90 +9,85 @@ import Button from "react-bootstrap/Button";
 import Map from "./Map";
 
 function Quiz() {
-  const continent = useParams().continent;
+	const continent = useParams().continent;
 
-  const [country, setCountry] = useState("");
-  const [questionCount, setQuestionCount] = useState(1);
+	const [country, setCountry] = useState("");
+	const [questionCount, setQuestionCount] = useState(1);
 
-  const countryArr = useRef(null);
-  useEffect(() => {
-    API.getCountryByContinent(continent).then((res) => {
-      countryArr.current = res.data; // Country Array from response
+	const countryArr = useRef(null);
+	useEffect(() => {
+		API.getCountryByContinent(continent).then((res) => {
+			countryArr.current = res.data; // Country Array from response
 
-      getRandomCountry();
-    });
+			getRandomCountry();
+		});
 
-    //TODO: Make call gto map api to retrieve the map for the given continent
-  }, []);
+		//TODO: Make call gto map api to retrieve the map for the given continent
+	}, []);
 
+	//Retrieving random country from the array to ask the user.
+	function getRandomCountry() {
+		const randomCountry =
+			countryArr.current[Math.floor(Math.random() * countryArr.current.length)];
 
+		setCountry(randomCountry);
+		setQuestionCount(questionCount + 1);
+		//TODO: Add functionality to check for correct answer and record the score
+	}
 
-  //Retrieving random country from the array to ask the user.
-  function getRandomCountry() {
-    const randomCountry =
-      countryArr.current[Math.floor(Math.random() * countryArr.current.length)];
-
-    setCountry(randomCountry);
-    setQuestionCount(questionCount + 1);
-    //TODO: Add functionality to check for correct answer and record the score
-  }
-  
-
-  return (
-    <div className="container mt-5 p-4">
-      <div className="row text-center">
-        <div className="col-sm-12 title-container p-2">
-          <h2 className="continent-heading">{continent}</h2>
-        </div>
-      </div>
-      <div className="row pt-5">
-        <div className="col-sm-4">
-          <p className="question-container">Where is {country}</p>
-        </div>
-        <div className="col-sm-6 map-container">
-          <Map continent={continent} />    
-        </div>
-        <div className="col-sm-3">
-          Click your Answer on the Map
-          <div className="row click-container mb-5">
-            <div id="arrowAnim">
-              <div className="arrowSliding">
-                <div className="arrow"></div>
-              </div>
-              <div className="arrowSliding delay1">
-                <div className="arrow"></div>
-              </div>
-              <div className="arrowSliding delay2">
-                <div className="arrow"></div>
-              </div>
-              <div className="arrowSliding delay3">
-                <div className="arrow"></div>
-              </div>
-            </div>
-          </div>
-          <div className="row next-container m-4 p-5">
-            {questionCount <= 5 ? (
-              <Button
-                type="button"
-                className="btn btn-danger btn-lg"
-                onClick={getRandomCountry}
-              >
-                <h3>NEXT QUESTION</h3>
-              </Button>
-            ) : (
-                <Button
-                  type="button"
-                  className="btn btn-danger btn-lg"
-                  href="/results"
-                >
-                  <h3>VIEW RESULTS</h3>
-                </Button>
-              )}
-          </div>
-        </div>
-      </div>
-      </div>
-  );
+	return (
+		<div className="container mt-5 p-4">
+			<div className="row text-center">
+				<div className="col-sm-12 title-container p-2">
+					<h2 className="continent-heading">{continent}</h2>
+				</div>
+			</div>
+			<div className="row pt-5">
+				<div className="col-sm-4">
+					<p className="question-container">Where is {country}</p>
+				</div>
+				<div className="col-sm-6 map-container">
+					<Map continent={continent} />
+				</div>
+				<div className="col-sm-3">
+					Click your Answer on the Map
+					<div className="row click-container mb-5">
+						<div id="arrowAnim">
+							<div className="arrowSliding">
+								<div className="arrow"></div>
+							</div>
+							<div className="arrowSliding delay1">
+								<div className="arrow"></div>
+							</div>
+							<div className="arrowSliding delay2">
+								<div className="arrow"></div>
+							</div>
+							<div className="arrowSliding delay3">
+								<div className="arrow"></div>
+							</div>
+						</div>
+					</div>
+					<div className="row next-container m-4 p-5">
+						{questionCount <= 5 ? (
+							<Button
+								type="button"
+								className="btn btn-danger btn-lg"
+								onClick={getRandomCountry}>
+								<h3>NEXT QUESTION</h3>
+							</Button>
+						) : (
+							<Button
+								type="button"
+								className="btn btn-danger btn-lg"
+								href="/results">
+								<h3>VIEW RESULTS</h3>
+							</Button>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Quiz;
