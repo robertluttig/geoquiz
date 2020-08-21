65703aa080ef596fb6bcc6cb26d12c0e01b2ad26
Janani NA <janani2285@gmail.com>
2020-08-20 17:07:51 -0700

    displaying marker on map click

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -19,15 +19,13 @@ var customStyled = [
 
 const GOOGLE_MAP_API = "AIzaSyBcjTAI9kaG51E3Yuqh5HnTCz_k042szLk";
 export class MapContainer extends Component {
-    // const[location, setLocation] = useState({ });
+    
     state = {
         longitude: 0,
-        latitude: 0
+        latitude: 0,
+        markerLocation:{}
     }
-    markerState = {
-        longitude: 0,
-        latitude: 0
-    }
+
     componentDidMount() {
         Geocode.setApiKey(GOOGLE_MAP_API);
         Geocode.setLanguage("en");
@@ -35,7 +33,7 @@ export class MapContainer extends Component {
             response => {
                 const { lat, lng } = response.results[0].geometry.location;
                 this.setState({ latitude: lat, longitude: lng })
-                console.log(lat, lng);
+            
             },
             error => {
                 console.error(error);
@@ -43,11 +41,8 @@ export class MapContainer extends Component {
         );
     }
     displayMarker = (mapProps, map, clickEvent) =>  {
-        console.log(clickEvent.latLng.lat())
-        console.log(clickEvent.latLng.lng())
-        this.markerState.latitude = clickEvent.latLng.lat()
-        this.markerState.longitude = clickEvent.latLng.lng()
-        console.log(clickEvent)
+     this.setState({...this.state, markerLocation: {lat:clickEvent.latLng.lat(), lng:clickEvent.latLng.lng()}})
+     
     };
     render() {
         return (
@@ -63,7 +58,7 @@ export class MapContainer extends Component {
             }}
                 onClick={this.displayMarker}
             >
-                <Marker position={{ lat: this.markerState.latitude, lng: this.markerState.longitude }} />
+                <Marker position={{ lat: this.state.markerLocation.lat, lng: this.state.markerLocation.lng }} />
             </Map>
         );
     }
